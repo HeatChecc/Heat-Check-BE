722570e86f9dc1a48ecd1e35c54f2e56c1091694
Eli Sachs <easachs13@gmail.com>
2022-10-17 11:52:34 -0600

    CI: fix bundler issue

--- spec/graphql/mutations/reviews/update_review_spec.rb
+++ spec/graphql/mutations/reviews/update_review_spec.rb
@@ -1,42 +1,40 @@
 require 'rails_helper'
 
-# module Mutations
-#   module Reviews
-#     RSpec.describe UpdateReview, type: :request do
-      # describe '.resolve' do
-      #   it 'updates a review' do
-      #     @dish_1 = Dish.create!(name: "Pad Thai", cuisine_type: "Thai", yelp_id: "1")
-      #     @user_1 = User.create!(username: "phil", email: "phil@phil.com")
-      #     @review_1 = Review.create!(description: "yummers", overall_rating: 4, user_id: @user_1.id, dish_id: @dish_1.id)
+module Mutations
+  module Reviews
+    RSpec.describe UpdateReview, type: :request do
+      describe '.resolve' do
+        it 'updates a review' do
+          @dish_1 = Dish.create!(name: "Pad Thai", cuisine_type: "Thai", yelp_id: "1", spice_rating: 3)
+          @user_1 = User.create!(username: "phil", email: "phil@phil.com")
+          @review_1 = Review.create!(description: "yummers", overall_rating: 4, user_id: @user_1.id, dish_id: @dish_1.id)
           
 
-      #     post '/graphql', params: { query: query }
+          post '/graphql', params: { query: query }
           
-      #     json = JSON.parse(response.body)
-      #     data = json['data']
-          
-      #     expect(data['review']['description']).to eq('eh, actually its not great')
-      #     expect(data['review']['overall_rating']).to eq(2)
-      #     expect(User.find_by(email: 'phil@phil.com').username).to eq('superphil')
-      #   end
+          json = JSON.parse(response.body)
+          data = json['data']
+          expect(data['review']['description']).to eq('eh, actually its not great')
+          expect(data['review']['overallRating']).to eq(2)
+        end
 
-      #   def query
-      #     <<~GQL
-      #     mutation {
-      #       review: updateReview(
-      #         input: {
-      #           id: "#{@review_1.id}"
-      #           description: "eh, actually its not great"
-      #           overall_rating: "2"
-      #         }
-      #         ) {
-      #           description
-      #           overall_rating
-      #         }
-      #       }
-      #     GQL
-      #   end
-#       end
-#     end
-#   end
-# end
+        def query
+          <<~GQL
+          mutation {
+            review: updateReview(
+              input: {
+                id: "#{@review_1.id}"
+                description: "eh, actually its not great"
+                overallRating: 2
+              }
+              ) {
+                description
+                overallRating
+              }
+            }
+          GQL
+        end
+      end
+    end
+  end
+end
