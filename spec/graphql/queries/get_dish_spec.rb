7dc475be06620ab42d7dd8460abf3f04c6ccc91b
Ethan Nguyen <etnguyen16@gmail.com>
2022-10-18 14:28:17 -0600

    add get dish spec
    co-authored-by: Gauri J.
     gauri728@gmail.com
     co-authored-by: Ethan N.
     etnguyen16@gmail.com
    co-authored-by: Eli S.
     easachs13@gmail.com
    co-authored-by: Phillip S.
    philmark18@gmail.com

---
+++ spec/graphql/queries/get_dish_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType, type: :request do
+  before do
+    @dish = Dish.create!(name: 'pad thai', cuisine_type: 'thai', yelp_id: 'OT6MJNr8Gzd9nyf25dEl6g', spice_rating: 3)
+  end
+  describe '.dish(id:)' do
+    it 'can query a single dish' do
+      post '/graphql', params: { query: query }
+      json = JSON.parse(response.body)
+      data = json['data']['dish']
+      expect(data['name']).to eq('pad thai')
+      expect(data['cuisineType']).to eq('thai')
+      expect(data['yelpId']).to eq('OT6MJNr8Gzd9nyf25dEl6g')
+      expect(data['spiceRating']).to eq(3)
+    end
+  end
+
+  def query
+    <<~GQL
+      { dish(id: "#{@dish.id}") {
+          name
+          cuisineType
+          yelpId
+          spiceRating
+        }
+      }
+    GQL
+  end
+end
