1838cce5ced22b2bfc51849186df1cf5d43271bf
Ethan Nguyen <etnguyen16@gmail.com>
2022-10-18 14:29:20 -0600

    add get user review spec test
    co-authored-by: Gauri J.
     gauri728@gmail.com
     co-authored-by: Ethan N.
     etnguyen16@gmail.com
    co-authored-by: Eli S.
     easachs13@gmail.com
    co-authored-by: Phillip S.
    philmark18@gmail.com

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
-
 class Dish < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates_presence_of :cuisine_type
   validates_presence_of :yelp_id
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
+
 end
