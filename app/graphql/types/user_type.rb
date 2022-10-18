05c5d0d74432b46830803792abcf7d12444e7ee7
Ethan Nguyen <etnguyen16@gmail.com>
2022-10-18 14:26:14 -0600

    edit user ttype rb
    co-authored-by: Gauri J.
     gauri728@gmail.com
     co-authored-by: Ethan N.
     etnguyen16@gmail.com
    co-authored-by: Eli S.
     easachs13@gmail.com
    co-authored-by: Phillip S.
    philmark18@gmail.com

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :username, String
     field :email, String
+    field :reviews, [Types::ReviewType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
