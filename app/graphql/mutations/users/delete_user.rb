feffc0c4a47f4595f97cad62175cd74b5ccb8ecf
Gauri Joshi <gauri728@gmail.com>
2022-10-19 10:16:13 -0700

    delete user mutation included in mutation resolve

---
+++ app/graphql/mutations/users/delete_user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Users
+    class DeleteUser < ::Mutations::BaseMutation
+      argument :id, ID, required: true
+      type Types::UserType
+
+      def resolve(attributes)
+        user = User.where(id: attributes[:id]).first
+        user.delete
+      end
+    end
+  end
+end
