28c5179a587c6f214ba1a79f71c1f987eb362a78
Ethan Nguyen <etnguyen16@gmail.com>
2022-10-20 15:41:46 -0600

    add create dish reviews mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,13 +2,13 @@
 
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
     field :create_user, mutation: Mutations::Users::CreateUser
     field :update_user, mutation: Mutations::Users::UpdateUser
     field :delete_user, mutation: Mutations::Users::DeleteUser
     field :create_dish, mutation: Mutations::Dishes::CreateDish
     field :delete_dish, mutation: Mutations::Dishes::DeleteDish
     field :update_dish, mutation: Mutations::Dishes::UpdateDish
+    field :create_dish_reviews, mutation: Mutations::Dishes::CreateDishReviews
     field :create_review, mutation: Mutations::Reviews::CreateReview
     field :update_review, mutation: Mutations::Reviews::UpdateReview
     field :delete_review, mutation: Mutations::Reviews::DeleteReview
