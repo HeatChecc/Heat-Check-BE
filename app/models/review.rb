e05a45591773c2fd6c5e4119fc1ecd5cc163eb82
Gauri Joshi <gauri728@gmail.com>
2022-10-20 19:21:19 -0700

    model method & test Review#highest_rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,8 @@ class Review < ApplicationRecord
   validates_presence_of :overall_rating
   belongs_to :user
   belongs_to :dish
+
+  def self.highest_rating 
+    Review.order(overall_rating: :desc)
+  end
 end
